1560e36188804a18c4bf95978cb0c348432baaf0
Hasina6 <raffiudeen9994890057@gmail.com>
2016-10-21 12:08:33 +0530

    Create pro10.c

---
+++ pro10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int number,remainder,count;
+printf("Enter the integer\n");
+scanf("%d",&number);
+for(count=0;number>0;number=number/10)
+{
+count=count+1;
+}
+printf("Number of digits of an integer is:%d,count");
+return 0;
+}
