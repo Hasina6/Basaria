4ddad49c5ea75117289fe49a40cc525ae7fca3fb
Hasina6 <raffiudeen9994890057@gmail.com>
2016-10-21 12:20:33 +0530

    Update pro10.c

--- pro10.c
+++ pro10.c
@@ -8,6 +8,6 @@ for(count=0;number>0;number=number/10)
 {
 count=count+1;
 }
-printf("Number of digits of an integer is:%d",count");
+printf("Number of digits of an integer is:%d",count);
 return 0;
 }
